79a6aaf86dec9732d76e907e285187f5dadfdd9f
YinJiaJin <yinjiajin@xiaomi.com>
2015-12-22 12:13:44 +0800

    redifine command line args, race proxy implemented

--- dns_reverse_proxy.go
+++ dns_reverse_proxy.go
@@ -18,39 +18,84 @@ However, a query for subdomain.example.com will go to 8.8.4.4:53.
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 	"regexp"
+	"runtime/debug"
 	"strings"
+	"sync"
+	"syscall"
+	"time"
 
-	"github.com/miekg/dns"
+	"github.com/wheelcomplex/dns"
+	"github.com/wheelcomplex/preinit/getopt"
+	"github.com/wheelcomplex/preinit/misc"
 )
 
+// command line parser
+
 var (
-	address = flag.String("address", ":53", "Address to listen to (TCP and UDP)")
+	opt = getopt.NewDefaultOpts()
 
-	defaultServer = flag.String("default", "",
-		"Default DNS server where to send queries if no route matched (IP:port)")
+	srvListenAddr = opt.OnceString("--listen", "127.0.0.1:53", "Address to listen to (TCP and UDP)")
 
-	routeList = flag.String("route", "",
+	upstreamServers = opt.OnceStringList("--upstreams", "8.8.8.8:53,8.8.4.4:53,119.29.29.29:53",
+		"Upstream DNS server where to send queries if no route matched (IP:port)")
+
+	defaultServer string = ""
+
+	// note: Bool option always default to false
+	noraceproxy = opt.OnceBool("--norace", "disable race mode, send queries to first upstream only, by default, proxy send queries to all upstreams and use the first response")
+
+	needhelp = opt.OnceBool("--help/-h", "show this usage")
+
+	debuglevel = opt.OnceInt("--debug", "0", "debug level(0-8)")
+
+	routeList = opt.OnceString("--routes", "*.google.com=8.8.8.8:53,*.facebook.com=8.8.8.8:53",
 		"List of routes where to send queries (subdomain=IP:port)")
 	routes map[string]string
 
-	allowTransfer = flag.String("allow-transfer", "",
+	allowTransfer = opt.OnceString("allow-transfer", "127.0.0.1",
 		"List of IPs allowed to transfer (AXFR/IXFR)")
 	transferIPs []string
 )
 
 func main() {
-	flag.Parse()
-	if *defaultServer == "" {
-		log.Fatal("-default is required")
+
+	if needhelp {
+		opt.Usage()
+		os.Exit(0)
 	}
-	transferIPs = strings.Split(*allowTransfer, ",")
+
+	//
+	for idx, _ := range upstreamServers {
+		upstreamServers[idx] = strings.Trim(upstreamServers[idx], ":")
+		if strings.Contains(upstreamServers[idx], ":") == false {
+			upstreamServers[idx] = upstreamServers[idx] + ":53"
+		}
+	}
+
+	//
+	if len(upstreamServers) > 0 {
+		defaultServer = upstreamServers[0]
+	}
+	if defaultServer == "" {
+		log.Fatal("--upstreams is required")
+		opt.Usage()
+		os.Exit(1)
+	}
+	if srvListenAddr == "" {
+		log.Fatal("--listen is required")
+		opt.Usage()
+		os.Exit(1)
+	}
+	transferIPs = strings.Split(allowTransfer, ",")
 	routes = make(map[string]string)
-	if *routeList != "" {
-		for _, s := range strings.Split(*routeList, ",") {
+	if routeList != "" {
+		for _, s := range strings.Split(routeList, ",") {
 			s := strings.SplitN(s, "=", 2)
 			if len(s) != 2 {
 				log.Fatal("invalid -routes format")
@@ -62,13 +107,39 @@ func main() {
 		}
 	}
 
-	udpServer := &dns.Server{Addr: *address, Net: "udp"}
-	tcpServer := &dns.Server{Addr: *address, Net: "tcp"}
+	// install signal USR1 for debug
+	sigCh := make(chan os.Signal, 128)
+	signal.Notify(sigCh, syscall.SIGUSR1)
+
+	go func() {
+		sig := <-sigCh
+		if sig == syscall.SIGUSR1 {
+			if debuglevel >= 8 {
+				fmt.Printf("\n------ runtime stack start %v ------\n", time.Now())
+				debug.PrintStack()
+				fmt.Printf("\n------ runtime stack end ------\n")
+			}
+		}
+	}()
+	if debuglevel >= 1 {
+		fmt.Printf("[%d]dns proxy listen(TCP+UDP) %s, upstreams %s ...\n", os.Getpid(), srvListenAddr, misc.ArgsToList(upstreamServers))
+		if noraceproxy {
+			fmt.Printf("race mode disabled.\n")
+		} else {
+			fmt.Printf("race mode enabled.\n")
+		}
+		fmt.Printf("debug level: %d\n", debuglevel)
+	}
+	udpServer := &dns.Server{Addr: srvListenAddr, Net: "udp"}
+	tcpServer := &dns.Server{Addr: srvListenAddr, Net: "tcp"}
 	dns.HandleFunc(".", route)
 	go func() {
 		log.Fatal(udpServer.ListenAndServe())
 	}()
 	log.Fatal(tcpServer.ListenAndServe())
+	//
+	// TODO: LRU cache
+	//
 }
 
 func route(w dns.ResponseWriter, req *dns.Msg) {
@@ -82,7 +153,11 @@ func route(w dns.ResponseWriter, req *dns.Msg) {
 			return
 		}
 	}
-	proxy(*defaultServer, w, req)
+	if noraceproxy {
+		proxy(defaultServer, w, req)
+	} else {
+		mproxy(upstreamServers, w, req)
+	}
 }
 
 func isTransfer(req *dns.Msg) bool {
@@ -148,4 +223,113 @@ func proxy(addr string, w dns.ResponseWriter, req *dns.Msg) {
 		return
 	}
 	w.WriteMsg(resp)
+}
+
+// send request to multi-dns-server and use the first response
+func mproxy(addrs []string, w dns.ResponseWriter, req *dns.Msg) {
+	transport := "udp"
+	if _, ok := w.RemoteAddr().(*net.TCPAddr); ok {
+		transport = "tcp"
+	}
+	if len(req.Question) == 0 {
+		if debuglevel >= 2 {
+			fmt.Printf("race proxying request from %s, upstream %s, transport %s, empty question discarded: \n%s\n", w.RemoteAddr().String(), misc.ArgsToList(addrs), transport, req.String())
+		}
+		return
+	}
+	if debuglevel >= 2 {
+		fmt.Printf("race proxying request from %s, upstream %s, transport %s: %s\n", w.RemoteAddr().String(), misc.ArgsToList(addrs), transport, req.Question[0].String())
+	} else if debuglevel >= 3 {
+		fmt.Printf("race proxying request from %s, upstream %s, transport %s: \n%s\n", w.RemoteAddr().String(), misc.ArgsToList(addrs), transport, req.String())
+	}
+	if isTransfer(req) && len(addrs) > 0 {
+		addr := addrs[0]
+		if transport != "tcp" {
+			dns.HandleFailed(w, req)
+			return
+		}
+		t := new(dns.Transfer)
+		c, err := t.In(req, addr)
+		if err != nil {
+			dns.HandleFailed(w, req)
+			return
+		}
+		if err = t.Out(w, req, c); err != nil {
+			dns.HandleFailed(w, req)
+			return
+		}
+		return
+	}
+
+	//
+	type result struct {
+		resp *dns.Msg
+		addr string
+		err  error
+	}
+
+	respCh := make(chan *result, len(addrs))
+	var mutex sync.Mutex
+	var responsed uint64 = 0
+	for _, addr := range addrs {
+		go func(addr string) {
+			if debuglevel >= 5 {
+				fmt.Printf("launch race proxying request from %s, upstream %s, transport %s: \n%s\n", w.RemoteAddr().String(), addr, transport, req.String())
+			}
+			c := &dns.Client{Net: transport}
+			resp, _, err := c.Exchange(req, addr)
+			mutex.Lock()
+			defer mutex.Unlock()
+			raceresp := &result{
+				resp: resp,
+				addr: addr,
+				err:  err,
+			}
+			if err != nil {
+				if debuglevel >= 5 {
+					fmt.Printf("race proxy from %s, upstream %s failed: %s\n", w.RemoteAddr().String(), addr, err.Error())
+				}
+				respCh <- raceresp
+				return
+			}
+			if responsed == 0 {
+				responsed++
+				if debuglevel >= 5 {
+					fmt.Printf("return response to %s, upstream %s: \n%s\n", w.RemoteAddr().String(), addr, resp.String())
+				}
+				respCh <- raceresp
+				return
+			}
+			// discard response
+			if debuglevel >= 5 {
+				fmt.Printf("discard response from %s, upstream %s: \n%s\n", w.RemoteAddr().String(), addr, resp.String())
+			}
+			return
+		}(addr)
+	}
+	// debug.PrintStack()
+	errcnt := 0
+	for raceresp := range respCh {
+		if raceresp.err != nil {
+			errcnt++
+			if errcnt >= len(addrs) {
+				if debuglevel >= 2 {
+					fmt.Printf("race proxying for %s failed finally: %s\n", w.RemoteAddr().String(), raceresp.err.Error())
+				}
+				// all server failed
+				dns.HandleFailed(w, req)
+				return
+			}
+			continue
+		}
+		w.WriteMsg(raceresp.resp)
+		if debuglevel >= 2 {
+			fmt.Printf("return response to %s, upstream %s finally: %s\n", w.RemoteAddr().String(), raceresp.addr, raceresp.resp.Answer[0].String())
+		} else if debuglevel >= 3 {
+			fmt.Printf("return response to %s, upstream %s finally: \n%s\n", w.RemoteAddr().String(), raceresp.addr, raceresp.resp.String())
+		}
+		return
+	}
+	// never reach here
+	panic("never reach here")
 }
